96f98aecd5d0206ec493e67d6c394e7cc4426106
esther.huang <esther.huang@titansoft.com.sg>
2024-07-04 09:42:09 +0800

    chore: git ignore

--- DesignPattern/obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
+++ DesignPattern/obj/Debug/net8.0/DesignPattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431280ec056b3d73adf9f559cf78a96336c7e053")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesignPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
